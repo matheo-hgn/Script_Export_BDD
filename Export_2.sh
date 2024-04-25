3021a2d1200355565d95b3fb29acd7afbac5c0da
Matheo HUGON <125356456+matheo-hgn@users.noreply.github.com>
2024-04-25 10:54:11 +0200

    Add files via upload

--- Export_2.sh
+++ Export_2.sh
@@ -15,22 +15,54 @@ read DB_NAME
 echo "Quel nom voulez vous donnez a l'export ? :"
 read EXPORT
 
+DATE=$(date +"%d-%m-%Y")
+
+NM_EXPORT="$DATE"_"$EXPORT".sql
+
+echo "1. Créer un dossier pour la sauvegarde /n2. Utiliser un dossier existant "
+read -r choix
+
+if [ "$choix" = "1"]; then
+    echo "Quel est le chemin du dossier (Entrer un chemin)"
+    read FOLDER
+
+    if [ -z "$FOLDER"]; then
+    	echo "chemin invalide, Le dossier ne peut être crée"
+    fi
+
+    sudo mkdir -p "$FOLDER"
+
+    if [ $FOLDER -eq 0 ]; then
+    	echo "Le dossier a était créé avec succés dans : $FOLDER "
+    else
+    	echo "le fichier n'a pas pu être crée"
+    fi
+
+
+elif ["$choix" = "2"]; then
+    echo "Quel est la racine du dossier existant ? :"
+    read FOLDER
+
+    if [ -z "$FOLDER" ]; then
+	echo "Chemin invalide, merci de rentrer un chemin existant"
+    fi
+
+fi
+
 
 DATE=$(date +"%d-%m-%Y")
 
 NM_EXPORT="$DATE"_"$EXPORT".sql
 
 
-mkdir -p  /var/backup/MYSQL/GLPI
 
-mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" > /var/backup/MYSQL/GLPI/"$NM_EXPORT"
-
+mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" > "$FOLDER"
 
 
-if [[ -f /var/backup/MYSQL/GLPI/$NM_EXPORT ]]; then
+
+if [[ -f "$FOLDER/$NM_EXPORT"]]; then
     echo "La base de donnée $DB_NAME a été exporter avec succès "
 else
     echo "une erreur est intervenu dans l'export de la base de données"
 fi
-
 
